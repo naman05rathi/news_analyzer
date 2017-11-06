7f06ddab95229f7de85f440209585a70d73f4d58
naman <official.naman05@gmail.com>
2017-11-07 02:16:22 +0530

    Updated article.py

--- article.py
+++ article.py
@@ -4,15 +4,24 @@ with open('links.txt') as f:
     link_list = f.read().splitlines()
 
 length = len(link_list)
-
-for i in range(length):
+i = 6
+for i in range(7):
 	url = link_list[i]
 	g = Goose()
 	article = g.extract(url=url)
 	thefile = open(str(i), 'w')
-	thefile.write(article.title.encode('ascii', 'ignore'))
-	thefile.write(article.meta_description.encode('ascii', 'ignore'))
-	thefile.write(article.cleaned_text.encode('ascii', 'ignore'))
+	title = article.title.encode('ascii', 'ignore')
+	desc = article.meta_description.encode('ascii', 'ignore')
+	text = article.cleaned_text.encode('ascii', 'ignore')
+	title_split = title.split(" ")
+	desc_split = desc.split(" ")
+	text_split = text.split(" ")
+	for word in title_split:
+		thefile.write(word+"\n")
+	for word in desc_split:
+		thefile.write(word+"\n")
+	for word in text_split:
+		thefile.write(word+"\n")
 
 # for link in link_list:
 # 	
